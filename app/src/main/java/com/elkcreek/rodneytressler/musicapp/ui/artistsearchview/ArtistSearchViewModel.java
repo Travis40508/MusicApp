70a03cd086fb65e25a8bee53ccd2798f5204e03f
Travis Tressler <r.travis.tressler@gmail.com>
2018-09-27 21:57:38 -0400

    Tidying up search.

--- app/src/main/java/com/elkcreek/rodneytressler/musicapp/ui/artistsearchview/ArtistSearchViewModel.java
+++ app/src/main/java/com/elkcreek/rodneytressler/musicapp/ui/artistsearchview/ArtistSearchViewModel.java
@@ -77,6 +77,7 @@ public class ArtistSearchViewModel extends ViewModel {
     }
 
     public void onArtistSearchTextChanged(CharSequence artistSearchText, int start, int before, int count) {
+        setShowProgressBar(false);
         if (timer != null) {
             timer.cancel();
         }
@@ -92,7 +93,7 @@ public class ArtistSearchViewModel extends ViewModel {
                 public void run() {
                     disposable.add(getArtistSearchResults(artistSearchText.toString()).subscribe(getSearchResponse(), updateUiWithError()));
                 }
-            }, 1000);
+            }, 750);
         }
     }
 
