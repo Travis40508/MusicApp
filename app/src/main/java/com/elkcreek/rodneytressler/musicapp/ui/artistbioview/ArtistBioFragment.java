159b7d19a5b6a0269ccd1b4bb0e55dc3326cdeec
Travis Tressler <r.travis.tressler@gmail.com>
2018-09-30 13:26:49 -0400

    Cleaned up classes. Will retain state last after refactor is finished.

--- app/src/main/java/com/elkcreek/rodneytressler/musicapp/ui/artistbioview/ArtistBioFragment.java
+++ app/src/main/java/com/elkcreek/rodneytressler/musicapp/ui/artistbioview/ArtistBioFragment.java
@@ -3,94 +3,36 @@ package com.elkcreek.rodneytressler.musicapp.ui.artistbioview;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.NestedScrollView;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions;
-import com.bumptech.glide.request.RequestOptions;
 import com.elkcreek.rodneytressler.musicapp.R;
 import com.elkcreek.rodneytressler.musicapp.databinding.FragmentArtistBioBinding;
-import com.elkcreek.rodneytressler.musicapp.repo.network.MusicApi;
 import com.elkcreek.rodneytressler.musicapp.ui.basefragment.BaseFragment;
 import com.elkcreek.rodneytressler.musicapp.ui.mainview.MainViewModel;
 import com.elkcreek.rodneytressler.musicapp.utils.Constants;
 import com.elkcreek.rodneytressler.musicapp.utils.SimilarArtistAdapter;
-
-import java.util.List;
-
 import javax.inject.Inject;
-
-import androidx.navigation.Navigation;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import dagger.android.support.AndroidSupportInjection;
 
-import static com.bumptech.glide.load.DecodeFormat.PREFER_ARGB_8888;
-import static com.elkcreek.rodneytressler.musicapp.utils.Constants.ARTIST_NAME_KEY;
-import static com.elkcreek.rodneytressler.musicapp.utils.Constants.ARTIST_UID_KEY;
+public class ArtistBioFragment extends BaseFragment {
 
-public class ArtistBioFragment extends BaseFragment implements ArtistBioView {
-
-//    @Inject
-//    protected ArtistBioPresenter presenter;
     @Inject
     protected ArtistBioFactory factory;
-    @BindView(R.id.image_artist_bio)
-    protected ImageView artistBioImage;
-    @BindView(R.id.text_artist_bio)
-    protected TextView artistBioText;
-    @BindView(R.id.text_read_more)
-    protected TextView readMoreText;
-    @BindView(R.id.bio_similar_artist_recycler_view)
-    protected RecyclerView similarArtistRecyclerView;
-    @BindView(R.id.text_similar_artists)
-    protected TextView similarArtistText;
-    @BindView(R.id.bio_scroll_view)
     protected NestedScrollView scrollView;
     private SimilarArtistAdapter adapter;
     private ArtistBioViewModel viewModel;
     private MainViewModel mainViewModel;
     private FragmentArtistBioBinding binding;
 
-    @OnClick(R.id.read_more_layout)
-    protected void onReadMoreClicked(View view) {
-//        presenter.readMoreClicked(readMoreText.getText().toString());
-    }
-
     @Override
     public void onAttach(Context context) {
         AndroidSupportInjection.inject(this);
         super.onAttach(context);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-//        presenter.artistRetrieved(getArguments().getString(Constants.ARTIST_UID_KEY));
-//        presenter.artistNameRetrieved(getArguments().getString(Constants.ARTIST_NAME_KEY));
-//        presenter.subscribe();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-//        presenter.unsubscribe();
-//        presenter.storeScrollViewState(scrollView.getScrollY());
     }
 
     @Nullable
@@ -101,15 +43,8 @@ public class ArtistBioFragment extends BaseFragment implements ArtistBioView {
     }
 
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-//        presenter.saveState(outState, scrollView != null ? scrollView.getScrollY() : 0);
-    }
-
-    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        presenter.getState(savedInstanceState);
         viewModel = ViewModelProviders.of(this, factory).get(ArtistBioViewModel.class);
         mainViewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
         viewModel.setMainViewModel(mainViewModel);
@@ -129,108 +64,5 @@ public class ArtistBioFragment extends BaseFragment implements ArtistBioView {
         ArtistBioFragment fragment = new ArtistBioFragment();
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void showArtistImage(String imageUrl) {
-        Glide.with(this)
-                .asBitmap()
-                .load(imageUrl)
-                .apply(RequestOptions.overrideOf(250, 300))
-                .apply(RequestOptions.encodeFormatOf(Bitmap.CompressFormat.PNG))
-                .apply(RequestOptions.formatOf(PREFER_ARGB_8888))
-                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.DATA))
-                .transition(BitmapTransitionOptions.withCrossFade())
-                .into(artistBioImage);
-    }
-
-    @Override
-    public void showArtistBio(String artistBio) {
-        artistBioText.setText(artistBio);
-//        presenter.bioShown();
-    }
-
-    @Override
-    public void setReadMoreText(String readMoreText) {
-        this.readMoreText.setText(readMoreText);
-    }
-
-    @Override
-    public void showSimilarArtists(List<MusicApi.Artist> artistList) {
-//        adapter = new SimilarArtistAdapter(Glide.with(this), artistList);
-//        similarArtistRecyclerView.setAdapter(adapter);
-//        similarArtistRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
-//        adapter.notifyDataSetChanged();
-//        adapter.setCallback(new SimilarArtistAdapter.BiosCallback() {
-//            @Override
-//            public void onSimilarArtistClicked(MusicApi.Artist artist) {
-////                presenter.similarArtistClicked(artist);
-//            }
-//        });
-    }
-
-    @Override
-    public void showSimilarArtistScreen(String artistUID, String artistName) {
-        Bundle bundle = new Bundle();
-        bundle.putString(ARTIST_UID_KEY, artistUID);
-        bundle.putString(ARTIST_NAME_KEY, artistName);
-        Navigation.findNavController(getView()).navigate(R.id.artistMainFragment, bundle);
-    }
-
-    @Override
-    public void setTitle(String artistName) {
-//        getActivity().setTitle(artistName);
-    }
-
-    @Override
-    public void hideMainProgressBar() {
-        hideMainLoadingLayout();
-    }
-
-    @Override
-    public void showLoadingLayout() {
-        this.showMainLoadingLayout();
-    }
-
-    @Override
-    public void showGenericArtistImage() {
-        Glide.with(this)
-                .asBitmap()
-                .load(R.drawable.generic_band)
-                .apply(RequestOptions.overrideOf(250, 300))
-                .apply(RequestOptions.encodeFormatOf(Bitmap.CompressFormat.PNG))
-                .apply(RequestOptions.formatOf(PREFER_ARGB_8888))
-                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.DATA))
-                .into(artistBioImage);
-    }
-
-    @Override
-    public void showNoSimilarArtistText() {
-        similarArtistText.setText(Constants.NO_SIMILAR_ARTISTS);
-    }
-
-    @Override
-    public void setImageBackgroundWhite() {
-        artistBioImage.setBackgroundColor(Color.WHITE);
-    }
-
-    @Override
-    public void setScrollViewPosition(int scrollYPosition) {
-        scrollView.post(new Runnable() {
-            @Override
-            public void run() {
-                scrollView.setScrollY(scrollYPosition);
-            }
-        });
-    }
-
-    @Override
-    public void toastConnectionFailedToast() {
-        Toast.makeText(getContext(), Constants.CONNECTION_ERROR, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void detachFragment() {
-        Navigation.findNavController(getView()).popBackStack();
     }
 }
--- app/src/main/java/com/elkcreek/rodneytressler/musicapp/ui/artistsearchview/ArtistSearchFragment.java
+++ app/src/main/java/com/elkcreek/rodneytressler/musicapp/ui/artistsearchview/ArtistSearchFragment.java
@@ -2,68 +2,26 @@ package com.elkcreek.rodneytressler.musicapp.ui.artistsearchview;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AutoCompleteTextView;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
 import com.elkcreek.rodneytressler.musicapp.R;
 import com.elkcreek.rodneytressler.musicapp.databinding.FragmentArtistSearchBinding;
-import com.elkcreek.rodneytressler.musicapp.repo.network.MusicApi;
 import com.elkcreek.rodneytressler.musicapp.ui.basefragment.BaseFragment;
 import com.elkcreek.rodneytressler.musicapp.ui.mainview.MainViewModel;
-import com.elkcreek.rodneytressler.musicapp.utils.EventHandlers;
-import com.elkcreek.rodneytressler.musicapp.utils.ArtistAdapter;
-import com.elkcreek.rodneytressler.musicapp.utils.Constants;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.inject.Inject;
-
-import androidx.navigation.Navigation;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnTextChanged;
 import dagger.android.support.AndroidSupportInjection;
 
-import static com.elkcreek.rodneytressler.musicapp.utils.Constants.ARTIST_NAME_KEY;
-import static com.elkcreek.rodneytressler.musicapp.utils.Constants.ARTIST_UID_KEY;
-
-public class ArtistSearchFragment extends BaseFragment implements ArtistSearchView {
+public class ArtistSearchFragment extends BaseFragment {
 
     @Inject ArtistSearchFactory factory;
-    @BindView(R.id.input_artist_search)
-    protected AutoCompleteTextView artistInput;
-    @BindView(R.id.text_search_value)
-    protected TextView searchText;
-//    @BindView(R.id.loading_layout)
-//    protected FrameLayout loadingLayout;
-//    private ArtistAdapter adapter;
-//    private LinearLayoutManager linearLayoutManager;
     private ArtistSearchViewModel viewModel;
     private MainViewModel mainViewModel;
     private FragmentArtistSearchBinding binding;
-
-    @OnTextChanged(value = R.id.input_artist_search, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
-    void onArtistSearchChange(Editable editable) {
-//        presenter.artistSearchTextChanged(editable.toString(), adapter != null && adapter.getItemCount() > 0);
-    }
-
 
     @Override
     public void onAttach(Context context) {
@@ -71,46 +29,11 @@ public class ArtistSearchFragment extends BaseFragment implements ArtistSearchVi
         super.onAttach(context);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-//        presenter.subscribe();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-//        presenter.unsubscribe();
-//        presenter.storeState(recyclerView.getLayoutManager() != null ? recyclerView.getLayoutManager().onSaveInstanceState() : null);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-//        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//            @Override
-//            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-////                presenter.scrollStateChanged(recyclerView.canScrollVertically(1));
-//            }
-//        });
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_artist_search, container, false);
-//        presenter.attachView(this);
-//        adapter = new ArtistAdapter(Glide.with(this), new ArrayList<>());
-//        recyclerView.setAdapter(adapter);
         return binding.getRoot();
-    }
-
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
     }
 
     public static ArtistSearchFragment newInstance() {
@@ -123,21 +46,8 @@ public class ArtistSearchFragment extends BaseFragment implements ArtistSearchVi
     }
 
     @Override
-    public void loadArtists(List<MusicApi.Artist> artistList) {
-//        adapter.setArtistList(artistList);
-//        presenter.listSet();
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-//        presenter.saveState(outState, recyclerView.getLayoutManager() != null ? recyclerView.getLayoutManager().onSaveInstanceState() : null);
-    }
-
-    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        presenter.getState(savedInstanceState);
         viewModel = getViewModel();
         mainViewModel = getMainViewModel();
         binding.setViewModel(viewModel);
@@ -151,92 +61,5 @@ public class ArtistSearchFragment extends BaseFragment implements ArtistSearchVi
 
     private MainViewModel getMainViewModel() {
         return ViewModelProviders.of(getActivity()).get(MainViewModel.class);
-    }
-
-    @Override
-    public void showProgressBar() {
-//        loadingLayout.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public void showMainProgressBar() {
-        showMainLoadingLayout();
-    }
-
-    @Override
-    public void hideProgressBar() {
-//        loadingLayout.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void showSearchTextValue(String searchText) {
-        this.searchText.setText("Showing Results For '" + searchText + "'");
-    }
-
-    @Override
-    public void showSearchTextTopArtists() {
-        this.searchText.setText(R.string.top_artists_text);
-    }
-
-    @Override
-    public void showErrorLoadingToast() {
-        Toast.makeText(getContext(), R.string.network_error_text, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void clearList() {
-//        adapter.clearList();
-    }
-
-    @Override
-    public void setActionBarTitle(String artistsTitle) {
-//        getActivity().setTitle(artistsTitle);
-    }
-
-    @Override
-    public void clearSearchText() {
-        artistInput.setText(Constants.EMPTY_TEXT);
-    }
-
-    @Override
-    public void setRecyclerViewPosition(Parcelable recyclerViewPosition) {
-//        recyclerView.getLayoutManager().onRestoreInstanceState(recyclerViewPosition);
-    }
-
-    @Override
-    public void detachFragment() {
-        getActivity().onBackPressed();
-    }
-
-
-    @Override
-    public void toastConnectionFailedToast() {
-        Toast.makeText(getContext(), Constants.CONNECTION_ERROR, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void scrollRecyclerViewToTop() {
-//        recyclerView.getLayoutManager().scrollToPosition(0);
-    }
-
-    @Override
-    public void addArtists(List<MusicApi.Artist> artists) {
-//        adapter.addToList(artists);
-    }
-
-    @Override
-    public void showMainArtistScreen(MusicApi.Artist artist) {
-        Bundle bundle = new Bundle();
-        bundle.putString(ARTIST_NAME_KEY, artist.getArtistName());
-        bundle.putString(ARTIST_UID_KEY, artist.getArtistUID());
-        Navigation.findNavController(getView()).navigate(R.id.artistMainFragment, bundle);
-    }
-
-
-    public void onArtistClicked(String artistName, String artistUid) {
-        Bundle bundle = new Bundle();
-        bundle.putString(ARTIST_NAME_KEY, artistName);
-        bundle.putString(ARTIST_UID_KEY, artistUid);
-        Navigation.findNavController(getView()).navigate(R.id.artistMainFragment, bundle);
     }
 }
